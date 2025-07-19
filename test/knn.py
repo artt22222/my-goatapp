4f78b692b89ec25bebd57f4dfde8535fc3d43b3b
Phat <phasri2547@gmail.com>
2025-07-19 20:15:50 +0700

    knn

--- test/knn.py
+++ test/knn.py
@@ -33,24 +33,3 @@ print(f" Precision : {pre:.2%}")
 print(f" Recall : {rec:.2%}")
 print(f" F1-Score : {f1:.2%}")
 print(classification_report(y_encode, y_pred, target_names=le.classes_))
-
-print("\n=== Confusion Report Per Class ===")
-cm = confusion_matrix(y_encode, y_pred)
-class_names = le.classes_
-
-for i, class_name in enumerate(class_names):
-    TP = cm[i, i]
-    FP = cm[:, i].sum() - TP
-    FN = cm[i, :].sum() - TP
-    TN = cm.sum() - (TP + FP + FN)
-
-    print(f"\nClass: {class_name}")
-    print(f"  True Positive  (TP): {TP}")
-    print(f"  False Positive (FP): {FP}")
-    print(f"  False Negative (FN): {FN}")
-    print(f"  True Negative  (TN): {TN}")
-
-
-#model.fit(x, y_encode)
-# jb.dump(model, 'test/RF_model.pkl')
-# jb.dump(le, 'test/label_encoder.pkl')
